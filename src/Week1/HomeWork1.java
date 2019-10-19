175cc0f6b56d376125c8142183fbfc8c0680f6b5
kitapawel <kitapawel@gmail.com>
2019-10-19 09:31:04 +0200

    Second commit from Intellij

--- src/Week1/HomeWork1.java
+++ src/Week1/HomeWork1.java
@@ -1,6 +1,6 @@
 package Week1;
 public class HomeWork1{
-	public static void main (String args[]){
+	public static void main (String[] args){
 		int i = 5;
 		while (i > 0){
 			System.out.println("Hello World!");
--- src/Week1/HomeWork2.java
+++ src/Week1/HomeWork2.java
@@ -1,6 +1,6 @@
 package Week1;
 public class HomeWork2{
-	public static void main(String args[]){
+	public static void main(String[] args){
 	int a = 75;
 	int b = 15;
 	int c = a+b;
